395fb2f27bfeb2a9bf6fc10d7ee4d777ee0f9662
Thiago Gabriel <thiago.gabriel051@gmail.com>
2023-01-12 13:57:37 -0300

    implementação do botão Load More

--- src/components/FilterByType/index.js
+++ src/components/FilterByType/index.js
@@ -2,7 +2,9 @@ import { useEffect, useState } from "react"
 import { Link } from "react-router-dom"
 import { pokemonAPI } from "../../services/api"
 import { Card } from "../Card"
+import { Button } from "../Button"
 import { Container } from "./style"
+import { QUANTITY_TO_LOAD } from "../../variables/variables"
 
 const PokemonInfo = ({ name }) => {
 
@@ -27,29 +29,47 @@ const PokemonInfo = ({ name }) => {
 
 export const FilterByType = ({ type }) => {
 
-    const [pokemons, setPokemonsList] = useState([])
+    const [pokemons, setPokemons] = useState([])
+    const [limit, setLimit] = useState([])
 
     useEffect(() => {
         const fetchData = async () => {
             const response = await pokemonAPI.getPokemonsByType(type)
-            setPokemonsList(
-                response.data.pokemon.map(pokemons => pokemons.pokemon.name)
-            )
+            setPokemons(response.data.pokemon.map(pokemons => pokemons.pokemon.name))
+            if (limit.length === 0) setLimit(pokemons.slice(0, QUANTITY_TO_LOAD))
         }
         fetchData()
-    }, [type])
+    }, [type, pokemons, limit.length])
+
+
+    const handleClick = () => {
+        if (limit.length <= pokemons.length - QUANTITY_TO_LOAD) {
+            setLimit(
+                pokemons.slice(0, (limit.length + QUANTITY_TO_LOAD))
+            )
+        } else {
+            setLimit(pokemons)
+        }
+
+        if (limit.length === pokemons.length) {
+            alert('All pokemons of this category have already been loaded!!')
+        }
+    }
 
     return (
-        <Container>
-            <ul>
-                {
-                    pokemons.map((pokemon, index) =>
-                        <li key={index}>
-                            <PokemonInfo name={pokemon} />
-                        </li>
-                    )
-                }
-            </ul>
-        </Container>
+        <>
+            <Container>
+                <ul>
+                    {
+                        limit.map((pokemon, index) =>
+                            <li key={index}>
+                                <PokemonInfo name={pokemon} />
+                            </li>
+                        )
+                    }
+                </ul>
+            </Container>
+            <Button onClick={handleClick}>Load More</Button>
+        </>
     )
 }
